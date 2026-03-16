2b35318f0e1c36b48b487f55807f2e8382ddc014
Kyle Cuss <cussprogramming@gmail.com>
2026-03-16 10:12:42 -0230

    Added starting scripts

---
+++ Source/CussAbilitySystem/CoreTypes/CussAbilityTypes.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CoreTypes/CussAbilityTypes.h"
+
+CussAbilityTypes::CussAbilityTypes()
+{
+}
+
+CussAbilityTypes::~CussAbilityTypes()
+{
+}
---
+++ Source/CussAbilitySystem/Data/CussAbilityData.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "CussAbilityData.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CUSSABILITYSYSTEM_API UCussAbilityData : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+	
+};
